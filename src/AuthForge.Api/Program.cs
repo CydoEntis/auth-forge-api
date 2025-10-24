a1f867554487fc3128e6632da2f77c36a2e445a7
Cody <cydoentis@gmail.com>
2025-10-24 19:56:32 -0400

    chore: remove API integration test project

--- src/AuthForge.Api/Program.cs
+++ src/AuthForge.Api/Program.cs
@@ -26,7 +26,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();  
+app.UseAuthentication();
 app.UseMiddleware<ApplicationIdentificationMiddleware>();
 app.UseAuthorization();
 
